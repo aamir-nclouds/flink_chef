e19bd160d70a0998702e41ace6954c86bfc14f36
aamir-nclouds <aamir@nclouds.com>
2020-07-30 03:43:22 +0500

    resource changed

--- recipes/flink.rb
+++ recipes/flink.rb
@@ -9,7 +9,7 @@ remote_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
 end
 
 
-execute 'extract' do
+execute 'extract_flink' do
     command 'tar zxvf /tmp/flink-1.11.1-bin-scala_2.11.tgz -C /'
     action :run
 end
