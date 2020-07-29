784989b1465614fc6b8937111e4f561b6dc077fa
aamir-nclouds <aamir@nclouds.com>
2020-07-30 01:12:56 +0500

    recipe name change to flink

---
+++ recipes/flink.rb
@@ -0,0 +1,54 @@
+package 'oracle-java7-installer' do
+    action :install
+end
+
+
+remote_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
+    source 'https://aamir-nclouds.s3.amazonaws.com/flink-1.11.1-bin-scala_2.11.tgz'
+    action :create
+end
+
+
+
+archive_file 'flink-1.11.1' do
+    path '/tmp/flink-1.11.1-bin-scala_2.11.tgz'
+    destination '/flink-1.11.1'
+    action :extract
+end
+
+execute 'flink' do
+    command 'sh /flink-1.11.1/flink-1.11.1/bin/start-cluster.sh'
+    action :run
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
