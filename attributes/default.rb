42eb4b53fafe4f6190c06c01bc9cf1078cf95585
aamir-nclouds <aamir@nclouds.com>
2020-08-04 03:20:03 +0500

    new recipe added

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['s3Bucket']['name'] = 'aamir-nclouds.s3.amazonaws.com'
 default['s3File']['name'] = 'flink-1.11.1-bin-scala_2.11.tgz'
+default['Flink_Updated_Pkg']['name'] = 'Updated-Flink.tgz'
---
+++ recipes/configure_flink.rb
@@ -0,0 +1,20 @@
+execute 'flink_stop' do
+    command 'sudo bash /flink-1.11.1/bin/stop-cluster.sh'
+    action :run
+end
+
+remote_file '/tmp/Updated-Flink.tgz' do
+    source "https://#{node['s3Bucket']['name']}/#{node['Flink_Updated_Pkg']['name']}"
+    mode '0777'
+    action :create
+end
+
+execute 'extract_flink' do
+    command 'tar zxvf /tmp/Updated-Flink.tgz -C / && mv /Updated-Flink/conf/flink-conf.yaml /flink-1.11.1/conf && mv /Updated-Flink/conf/log4j.properties /flink-1.11.1/conf'
+    action :run
+end
+
+execute 'flink_start' do
+    command 'sudo bash /flink-1.11.1/bin/start-cluster.sh'
+    action :run
+end
